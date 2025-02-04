8ad0004f9a5b17045652faa8213173b58be68012
GabrielC248 <gabrielccoelho10@gmail.com>
2025-02-04 16:43:25 -0300

    Diagrama + Código + Comentários

--- Tarefa_01/Tarefa_01.c
+++ Tarefa_01/Tarefa_01.c
@@ -1,37 +1,102 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
+#include "pico/cyw43_arch.h"
 #include "hardware/timer.h"
 #include "hardware/clocks.h"
-#include "pico/cyw43_arch.h"
 
-int64_t alarm_callback(alarm_id_t id, void *user_data) {
-    // Put your timeout handler code in here
-    return 0;
+// ---------------- Defines - Início ----------------
+
+// Definição dos pinos dos LEDs do semáforo
+#define red_pin 11
+#define yellow_pin 12
+#define green_pin 13
+
+// ---------------- Defines - Fim ----------------
+
+
+
+// ---------------- Variáveis - Início ----------------
+
+// Variável de controle para gerenciar o estado do semáforo
+static volatile uint ctrl = 0;
+
+// ---------------- Variáveis - Fim ----------------
+
+
+
+// ---------------- Semáforo - Início ----------------
+
+// Inicializa os pinos do semáforo e define o estado inicial (vermelho ligado)
+void init_semaforo() {
+  gpio_init(red_pin);
+  gpio_init(yellow_pin);
+  gpio_init(green_pin);
+
+  gpio_set_dir(red_pin,GPIO_OUT);
+  gpio_set_dir(yellow_pin,GPIO_OUT);
+  gpio_set_dir(green_pin,GPIO_OUT);
+
+  gpio_put(red_pin, 1);    // Vermelho ligado
+  gpio_put(yellow_pin, 0); // Amarelo desligado
+  gpio_put(green_pin, 0);  // Verde desligado
+
+  ctrl = 1; // Define o estado inicial do semáforo
+}
+
+// ---------------- Semáforo - Fim ----------------
+
+
+
+// Callback para a troca dos estados do semáforo em intervalos de tempo
+bool repeating_timer_callback(struct repeating_timer *t) {
+    switch(ctrl) {
+        case 0:
+            gpio_put(green_pin, 0);  // Desliga verde
+            gpio_put(red_pin, 1);    // Liga vermelho
+            ctrl++;
+            break;
+        case 1:
+            gpio_put(red_pin, 0);    // Desliga vermelho
+            gpio_put(yellow_pin, 1); // Liga amarelo
+            ctrl++;
+            break;
+        case 2:
+            gpio_put(yellow_pin, 0); // Desliga amarelo
+            gpio_put(green_pin, 1);  // Liga verde
+            ctrl = 0;
+            break;
+        default:
+            // Estado inválido: Reinicia o controle
+            gpio_put(red_pin, 1);
+            gpio_put(yellow_pin, 0);
+            gpio_put(green_pin, 0);
+            ctrl = 1;
+    }
+    return true;
 }
 
 int main()
 {
+    uint contador = 0; // Para contar os segundos passados da execução
+
     stdio_init_all();
 
-    // Initialise the Wi-Fi chip
-    if (cyw43_arch_init()) {
-        printf("Wi-Fi init failed\n");
-        return -1;
-    }
+    init_semaforo(); // Configura o semáforo
+    
+    // Configura um timer para chamar a função de troca de estados a cada 3 segundos
+    struct repeating_timer timer;
+    add_repeating_timer_ms(3000, repeating_timer_callback, NULL, &timer);
 
-    // Timer example code - This example fires off the callback after 2000ms
-    add_alarm_in_ms(2000, alarm_callback, NULL, false);
-    // For more examples of timer use see https://github.com/raspberrypi/pico-examples/tree/master/timer
-
-    printf("System Clock Frequency is %d Hz\n", clock_get_hz(clk_sys));
-    printf("USB Clock Frequency is %d Hz\n", clock_get_hz(clk_usb));
-    // For more examples of clocks use see https://github.com/raspberrypi/pico-examples/tree/master/clocks
-
-    // Example to turn on the Pico W LED
-    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
-
+    // Superloop: Imprime um contador de segundos continuamente
     while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
+        printf("Contador de segundos: %d s\n",contador);
+        
+        sleep_ms(1000); // Espera 1 segundo (1000 ms)
+
+        // Incrementa o contador e verifica se é múltiplo de 3
+        if( ( (++contador) % 3) == 0 ) {
+            printf("TEMPO! "); // Indica quando deve acontecer uma troca no semáforo
+        } 
     }
+    return 0;
 }
